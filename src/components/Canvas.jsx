1f69e6f6f2824e365e746b2e7b6189cb36edd4a6
Inzamul Haque <inzamul119@gmail.com>
2022-02-07 23:18:57 +0600

    create canvas component

---
+++ src/components/Canvas.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import style from '../styles/Canvas.module.css';
+
+function Canvas() {
+  return <div className={style.canvas} contentEditable >canvas</div>;
+}
+
+export default Canvas;
