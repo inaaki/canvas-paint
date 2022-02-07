bd636079ec19353809d8d24160cc520222b9153c
Inzamul Haque <inzamul119@gmail.com>
2022-02-07 22:51:18 +0600

    refactor: add <paint> to <app>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
-import Playground from './components/Playground';
+import React, { useState } from 'react';
+import Paint from './components/Paint';
+import style from './styles/App.module.css';
+import getRandomColor from './utils/randomColor';
+
+const INITIAL_COLOR = getRandomColor();
 
 function App() {
-  return <Playground />;
+  const [color, setColor] = useState(INITIAL_COLOR);
+
+  return (
+    <div>
+      <Paint color={color} />
+      <button
+        className={style.colorBtn}
+        type="button"
+        onClick={() => {
+          setColor(getRandomColor());
+        }}
+      >
+        Generate Color
+      </button>
+    </div>
+  );
 }
 
 export default App;
