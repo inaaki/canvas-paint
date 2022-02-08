6655c9d9bd90cb7cfe6fc2199c5494f2762c31c8
Inzamul Haque <inzamul119@gmail.com>
2022-02-08 19:43:08 +0600

    fix: useCallback to define function reference

--- src/components/WindowSize.jsx
+++ src/components/WindowSize.jsx
@@ -1,19 +1,20 @@
-import React, { useRef, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import useWindowSize from '../hooks/useWindowSize';
 import style from '../styles/WindowSize.module.css';
 
 function WindowSize() {
   const [show, setShow] = useState(true);
-
+  //
   const timeoutRef = useRef(0);
-
-  const [windowWidth, windowHeight] = useWindowSize(() => {
+  const callback = useCallback(() => {
     setShow(true);
     clearTimeout(timeoutRef.current);
     timeoutRef.current = setTimeout(() => {
       setShow(false);
     }, 1000);
-  });
+  }, []);
+  //
+  const [windowWidth, windowHeight] = useWindowSize(callback);
 
   if (!show) return null;
   return (
