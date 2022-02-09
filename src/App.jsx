5bc383358023a8da872d4066e6099b4a7d8e33aa
Inzamul Haque <inzamul119@gmail.com>
2022-02-09 12:56:58 +0600

    refactor: lifting state up with custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,31 @@
-import React, { useState } from 'react';
-import Canvas from './components/Canvas';
+import React, { useEffect, useState } from 'react';
 import Paint from './components/Paint';
 import WindowSize from './components/WindowSize';
+import useColorSet from './hooks/useColorSet';
 import style from './styles/App.module.css';
 import getRandomColor from './utils/randomColor';
 
 const INITIAL_COLOR = getRandomColor();
 
 function App() {
-  const [color, setColor] = useState(INITIAL_COLOR);
+  const [colorSet, setColorSet] = useColorSet(INITIAL_COLOR);
+  const [activeColor, setActiveColor] = useState('');
+  //
+  useEffect(() => {
+    if (colorSet.length) setActiveColor(colorSet[0]);
+  }, [colorSet]);
 
   return (
     <div className={style.container}>
-      <Paint color={color} />
-
-      <button
-        className={style.colorBtn}
-        type="button"
-        onClick={() => {
-          setColor(getRandomColor());
-        }}
-      >
+      <Paint
+        colors={colorSet}
+        activeColor={activeColor}
+        handleActive={setActiveColor}
+      />
+      <button className={style.colorBtn} type="button" onClick={setColorSet}>
         Generate Color
       </button>
-      <Canvas />
+      {/* <Canvas /> */}
       <WindowSize />
     </div>
   );
--- src/components/Paint.jsx
+++ src/components/Paint.jsx
@@ -1,45 +1,18 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import style from '../styles/Paint.module.css';
 import ColorPicker from './ColorPicker';
 
-function Paint({ color }) {
-  const [activeColor, setActiveColor] = useState('');
-  const [colors, setColors] = useState([]);
-
-  useEffect(() => {
-    // abort controller for fetch
-    const controller = new AbortController();
-    const { signal } = controller;
-    //
-    (() => {
-      const baseColor = color.slice(1);
-      fetch(
-        `https://www.thecolorapi.com/scheme?hex=${baseColor}&mode=monochrome&count=5`,
-        { signal, method: 'GET' },
-      )
-        .then((res) => res.json())
-        .then((res) => {
-          const result = res.colors.map((item) => item.hex.value);
-          setColors(result);
-          setActiveColor(res.colors[0].hex.value);
-        })
-        .catch(() => {});
-    })();
-
-    return () => {
-      controller.abort();
-    };
-  }, [color]);
+function Paint({ colors, activeColor, handleActive }) {
 
   return (
     <div className={style.container}>
-      {colors.map((item) => (
+      {colors?.map((item) => (
         <ColorPicker
           color={item}
           key={item}
           activeColor={activeColor}
-          onActive={() => setActiveColor(item)}
+          onActive={() => handleActive(item)}
         />
       ))}
     </div>
